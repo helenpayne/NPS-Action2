bdb615436ac119fa1650681756df99d29a091f35
helenpayne <63317442+helenpayne@users.noreply.github.com>
2021-11-19 03:09:25 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,17 +8,17 @@
     chmod +x npc
     sleep 1
     ./npc -version
-    whoami
     sleep 1
-    ./npc -server=150.230.12.211:8024 -vkey=3mh9rlvj7sgbmlhv -type=tcp
+    whoami
     sleep 1
     echo runner:sw456 |sudo chpasswd runner
     sleep 1
-    sudo sed -i 's/^.*PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
-    sleep 1
-    sudo sed -i 's/^.*PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
-    sleep 1
-    sudo service sshd restart
-    sleep 1
+    ./npc -server=150.230.12.211:8024 -vkey=3mh9rlvj7sgbmlhv -type=tcp
+    #sleep 1
+    #sudo sed -i 's/^.*PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
+    #sleep 1
+    #sudo sed -i 's/^.*PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+    #sleep 1
+    #sudo service sshd restart
     sleep 10h
     ./npc -version
