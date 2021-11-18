2017ef5bcad0691df0b5f5c72fe55eb5cc741844
helenpayne <63317442+helenpayne@users.noreply.github.com>
2021-11-19 01:25:07 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
-    curl -fsSL https://github.com/ehang-io/nps/releases/download/v0.26.10/linux_amd64_client.tar.gz -o nps.tar.gz
-    tar -zxvf nps.tar.gz nps
-    rm nps.tar.gz
-    chmod +x nps
-    sudo cd nps
-    nps -version
-    ./npc -server=150.230.12.211:8024 -vkey=3mh9rlvj7sgbmlhv -type=tcp
+    wget https://github.com/ehang-io/nps/releases/download/v0.26.10/linux_amd64_client.tar.gz
+    tar -zxvf linux_amd64_client.tar.gz
+    rm linux_amd64_client.tar.gz
+    ls
+    #chmod +x nps
+    #sudo cd nps
+    #nps -version
+    #./npc -server=150.230.12.211:8024 -vkey=3mh9rlvj7sgbmlhv -type=tcp
     
