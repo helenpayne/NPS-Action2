d292ea2752709dcb5e795ed4896c30e390b18d63
helenpayne <63317442+helenpayne@users.noreply.github.com>
2021-11-20 04:48:11 +0800

    Update ubuntu-start.sh

--- ubuntu-start.sh
+++ ubuntu-start.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt-get install build-essential asciidoc binutils bzip2 gawk gettext git subversion libssl-dev libncurses5-dev patch unzip zlib1g-dev libc6-dev libbz2-dev gdisk flex python
-git clone https://github.com/syb999/openwrt-15.05
+git clone https://github.com/sw586/openwrt-15.05
 cd openwrt-15.05
 ./scripts/feeds update -a
 ./scripts/feeds install -a
