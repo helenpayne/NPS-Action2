ecf6e3bf4f8c5e013973e6036c83dfc1772982f0
helenpayne <63317442+helenpayne@users.noreply.github.com>
2021-11-21 23:43:08 +0800

    Update ubuntu-start.sh

--- ubuntu-start.sh
+++ ubuntu-start.sh
@@ -5,3 +5,11 @@
 #./scripts/feeds update -a
 #./scripts/feeds install -a
 #touch target/linux/*/Makefile
+
+
+sudo apt update
+sudo apt-get install binutils bzip2 diff find flex gawk gcc-6+ getopt grep install libc-dev libz-dev make4.1+ perl python3.6+ rsync subversion unzip which
+git clone https://github.com/sw586/openwrt
+cd openwrt
+./scripts/feeds update -a
+./scripts/feeds install -a
