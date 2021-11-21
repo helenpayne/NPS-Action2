12cd11095fd6fee44d505c0b93981147b1486f2f
helenpayne <63317442+helenpayne@users.noreply.github.com>
2021-11-21 15:18:45 +0800

    Update ubuntu-start.sh

--- ubuntu-start.sh
+++ ubuntu-start.sh
@@ -6,9 +6,9 @@
 #./scripts/feeds install -a
 #touch target/linux/*/Makefile
 
-sudo apt-get update
-sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl swig rsyncgit clone https://github.com/sw586/openwrt
-git clone https://github.com/sw586/openwrt.git
+sudo apt update
+sudo apt -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl swig rsyncgit clone https://github.com/sw586/openwrt
+git clone https://github.com/sw586/openwrt
 cd openwrt
 ./scripts/feeds update -a
 ./scripts/feeds install -a
